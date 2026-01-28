29aad1666845daea39af26418af0ec77de896b47
Lasse Egebo <lae003@edu.zealand.dk>
2026-01-28 11:19:46 +0100

    Added temperature endpoint and hover tooltip that shows a country's co2 emissions

--- backend/app.py
+++ backend/app.py
@@ -15,16 +15,15 @@ co2_df=pd.read_csv("data/owid-co2-data.csv")
 temp_df=pd.read_csv("data/NASA_GISTEMP.csv", skiprows=1)
 
 @app.get("/api/co2")
-def get_co2(year: int, metric: str = "total"):
+def get_co2(year: int):
     df = co2_df[
         (co2_df["year"] == year) &
         (co2_df["iso_code"].notna()) &
         (co2_df["iso_code"].str.len() == 3)
     ]
     
-    column = "co2" if metric == "total" else "co2_per_capita"
-    result_df = df[["iso_code", column]].dropna()
-    result_df.columns = ["code", "value"]
+    result_df = df[["iso_code", "co2", "co2_per_capita"]].dropna()
+    result_df.columns = ["code", "co2", "co2_per_capita"]
         
     return result_df.to_dict(orient="records")
 
